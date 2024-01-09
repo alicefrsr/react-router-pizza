28ec194a82a88caa9aca4a6fb2cb2a65df614913
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2024-01-09 18:26:16 +0100

    fix responsive issues - reposition cart overview

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,10 +1,10 @@
 function Footer() {
   return (
     <div className="absolute bottom-0 ">
-      <div className="w-screen items-center justify-around bg-stone-800 px-4 py-4 text-center text-[10px] text-stone-200 xs:text-sm sm:px-6 lg:flex lg:text-base">
+      <div className="w-screen items-center justify-around bg-stone-800 py-2 text-center text-[11px] text-stone-200 xs:px-4 xs:py-4 xs:text-sm sm:px-6 lg:flex lg:text-base">
         {/* <p className="space-x-4 text-sm font-semibold text-stone-300 sm:space-x-4 md:text-base"> */}
         <p>Via Felice Cascione, 18100 Imperia, Italy </p>
-        <p>+39 0183 960396 </p>
+        <p className="text-[14px]">+39 0183 960396 </p>
         <p>Open between 12.00 - 22.00 </p>
         {/* </p> */}
       </div>
